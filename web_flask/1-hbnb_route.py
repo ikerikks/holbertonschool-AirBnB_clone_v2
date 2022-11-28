7b2456ba1f12949fe0550040a04f109b22a8977c
ikerikks <ikerikks@gmail.com>
2022-11-28 19:18:52 +0100

    start a web app

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""starts a web app"""
+""" starts a web app """
 
 from flask import Flask
 # from os import getenv
@@ -9,6 +9,7 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello_world():
     return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hello_world():
