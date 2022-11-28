4f041f1117ec44dfdb69e193305de3b1a1d4ff5c
ikerikks <ikerikks@gmail.com>
2022-11-28 19:25:08 +0100

    updating file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,7 +2,6 @@
 """starts a web app"""
 
 from flask import Flask
-from os import getenv
 app = Flask(__name__)
 
 
@@ -10,6 +9,7 @@ app = Flask(__name__)
 def hello_world():
     return "Hello HBNB!"
 
+
 if __name__ == "__main__":
-    port = int(getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+
+    app.run(host="0.0.0.0", port=5000)
