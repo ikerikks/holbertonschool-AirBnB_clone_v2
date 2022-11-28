a38a2a9932cc502463ad70fe57f51ccc2230ddbb
ikerikks <ikerikks@gmail.com>
2022-11-28 19:28:01 +0100

    renaming function

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,10 +6,9 @@ app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello_world():
+def hello_index():
     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-
     app.run(host="0.0.0.0", port=5000)
