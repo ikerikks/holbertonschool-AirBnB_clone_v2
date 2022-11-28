ef99843996f63bbb1c2d4374346afa9a2d48f2c5
ikerikks <ikerikks@gmail.com>
2022-11-28 19:06:49 +0100

    update of code style

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello_world():
     return "Hello HBNB!"
-    
+
 if __name__ == "__main__":
     port = int(getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
