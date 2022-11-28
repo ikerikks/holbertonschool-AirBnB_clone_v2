3a3e2377d74745a6ce8808177d8c1bcea646cbc8
ikerikks <ikerikks@gmail.com>
2022-11-28 19:34:07 +0100

    update 2

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -2,21 +2,19 @@
 """ starts a web app """
 
 from flask import Flask
-# from os import getenv
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello_world():
-    """diplay text"""
+def hello_index():
+    """ diplays text """
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
-def hello_world():
-    """diplay text"""
+def hbnb():
+    """ diplays text """
     return "HBNB"
 
-# if __name__ == "__main__":
-#     # port = int(getenv("PORT", 5000))
-#     app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == "__main__":
+     app.run(host="0.0.0.0", port=5000, debug=True)
