5933bd8cd275396a2d2bf01a53a2b68ac4350194
ikerikks <ikerikks@gmail.com>
2022-11-28 19:20:26 +0100

    start a web app update

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -8,11 +8,13 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_world():
+    """diplay text"""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hello_world():
+    """diplay text"""
     return "HBNB"
 
 # if __name__ == "__main__":
