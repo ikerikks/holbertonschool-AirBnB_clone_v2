8e420cfd403d620444bb8abbd0c4479b0a677940
ikerikks <ikerikks@gmail.com>
2022-11-29 13:31:58 +0100

    task4; is it a number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""" starts a web app """
+
+from flask import Flask, url_for
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_index():
+    """ diplays text """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ diplays text """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text):
+    """ displays text """
+    return "C " + text.replace("_", " ")
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def python_route(text):
+    """ displays text """
+    print(text)
+    return "Python " + text.replace("_", " ")
+
+
+@app.route("/python/", strict_slashes=False)
+def python(text="is cool"):
+    """ displays text """
+    print(text)
+    return "Python " + text.replace("_", " ")
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def is_number(n):
+    """ displays text """
+    return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
